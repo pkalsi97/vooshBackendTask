d6cab75a67f27d94de06145d2aea98473f2c484c
pkalsi97 <pranjulkalsi@gmail.com>
2024-05-24 13:57:02 +0530

    Minor bug fixes in server file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 
 const authRouter = require('./routes/authRoutes');
 const pageRouter = require('./routes/pageRoutes');
+const profileRouter = require('./routes/profileRoutes');
 
 const { sequelize } = require('./models');
 require('./utils/passport-setup');
@@ -19,10 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
-app.get('/', (req, res) => res.redirect('/login')); 
+app.get('/', (req, res) => res.redirect('/login'));
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/', profileRouter);
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
