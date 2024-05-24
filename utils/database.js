614119b217d1f0557317cdf66c73c3821864b1b8
pkalsi97 <pranjulkalsi@gmail.com>
2024-05-24 14:07:02 +0530

    Minor changes

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  port: process.env.DB_PORT,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+            ca: process.env.DB_CA_CERT ? [process.env.DB_CA_CERT] : undefined
+        }
     }
-  }
 });
 
 module.exports = sequelize;
